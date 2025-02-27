a9c7af0e22d5432c3bcc107740c2aa56d40fb473
Хомидов Амрулло <163350768+Amrull0@users.noreply.github.com>
2025-02-27 10:14:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Patient[] patients = new Patient[5];
+        patients[0] = new Patient("Иванов", "Иван", "Иванович", "Москва, ул. Ленина, 10", 12345, "Грипп");
+        patients[1] = new Patient("Петров", "Петр", "Петрович", "Пенза, пр. Строителей, 5", 67890, "Диабет");
+        patients[2] = new Patient("Сидоров", "Алексей", "Алексеевич", "Казань, ул. Баумана, 3", 11223, "Грипп");
+        patients[3] = new Patient("Козлова", "Анна", "Сергеевна", "Пенза, ул, Пушкина, 103", 24523, "Ангина");
+        patients[4] = new Patient("Козлова", "Ольга", "Васильевна", "Казань, ул. Победы, 67", 73487, "Рак");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите диагноз для поиска: ");
+        String searchDiagnos = scanner.nextLine();
+
+        System.out.println("Пациенты с диагнозом: " + searchDiagnos);
+        boolean found = false;
+
+        for (Patient patient : patients) {
+            if (patient.getDiagnos().equalsIgnoreCase(searchDiagnos)) {
+                System.out.println(patient.name + " " + patient.surname + ", Адрес: " + patient.address +
+                        ", № карты: " + patient.medCard + ", Диагноз: " + patient.diagnos);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Пациенты с таким диагнозом не найдены.");
+        }
+
+        scanner.close();
+    }
+}
+class Patient {
+    String name;
+    String surname;
+    String otchestvo;
+    String address;
+    int medCard;
+    String diagnos;
+
+    public Patient(String name, String surname, String otchestvo, String address, int medCard, String diagnos) {
+        this.name = name;
+        this.surname = surname;
+        this.otchestvo = otchestvo;
+        this.address = address;
+        this.medCard = medCard;
+        this.diagnos = diagnos;
+    }
+
+    public String getDiagnos() {
+        return diagnos;
+    }
+}
+
